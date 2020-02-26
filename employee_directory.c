9607ad91045c9397e839a04b0401c385c0651c99
Jeff Gradner <gradnerj@gmail.com>
2020-02-26 16:45:46 -0700

    Accepts pwd for modifying and changes running user to root if confirmed

--- employee_directory.c
+++ employee_directory.c
@@ -6,9 +6,9 @@
 void EmployeeDirectory(void){
   int selection = 0;
   FILE* directoryFile = NULL;
+  char* adminPassword = NULL;
 
   while(1){
-
       selection = 0;
       printf("\nEmployee Directory Portal:\n");
       printf("1. View Directory\n");
@@ -30,14 +30,44 @@ void EmployeeDirectory(void){
       }
       else if(selection == 2){
           printf("Modify Directory - Selected\n");
-          uid_t rootID = 0;
-          setuid(rootID);
+          printf("Please enter the super-secure administrative password: ");
+          adminPassword = malloc(25);
+          scanf("%s", adminPassword);
+          FILE* pwdFile = NULL;
+          char pwd[255];
 
-          uid_t runningUser = getuid();
+          pwdFile = fopen("password.txt", "r");
+
+          if(pwdFile == NULL){
+            printf("Error Opening File!\n");
+          }else{
+            printf("File Opened Succesfully!\n");
+          }
+        //  printf("HERE1\n");
+          fscanf(pwdFile, "%s", pwd);
+          //pwd = fgets(pwd, 255, pwdFile);
+          printf("%s is the actual password\n", pwd );
+          printf("%s is the entered password\n", adminPassword );
+
+
+
+          fclose(pwdFile);
+        //  printf("HERE2\n");
+          int equal = strcmp(adminPassword, pwd);
+
+          remove(adminPassword);
+          if(equal == 0){
+            uid_t rootID = 0;
+            setuid(rootID);
+
+            uid_t runningUser = getuid();
           uid_t ownerUser = geteuid();
 
-          printf("The running user is: %d\n", runningUser);
-          printf("The owner user is: %d\n", ownerUser);
+            printf("The running user is: %d\n", runningUser);
+            printf("The owner user is: %d\n", ownerUser);
+          }else{
+           printf("Incorrect Password.\n");
+         }
 
       }
       else if(selection == 3){
@@ -51,5 +81,6 @@ void EmployeeDirectory(void){
           printf("Invalid Selection! Try Again.\n");
 
       }
+      while((getchar()) != '\n');
   }
 }
--- makefile
+++ makefile
@@ -4,6 +4,7 @@ SimpleToolKit: main.o crypto.o employee_directory.o
 	gcc main.o crypto.o employee_directory.o -o SimpleToolKit
 	sudo chown root:root SimpleToolKit
 	sudo chmod 4755 SimpleToolKit
+	sudo chmod 644 directory.txt
 main.o: main.c
 	gcc -g -c main.c
 
